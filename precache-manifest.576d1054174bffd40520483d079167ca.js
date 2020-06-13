a4dfe33f4032fd6f9586b03349d95f5da1796ab1
kenodek <dominik.targosz@protonmail.com>
2020-06-13 09:41:39 +0200

    Updates

---
+++ precache-manifest.576d1054174bffd40520483d079167ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cc1fdd9dbb2039a11704b446e889d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca0061f64e0477b9ad78",
+    "url": "/static/css/main.f3bbba0f.chunk.css"
+  },
+  {
+    "revision": "c1ed95f9bc95f0489ff3",
+    "url": "/static/js/2.0ac883bb.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/static/js/2.0ac883bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca0061f64e0477b9ad78",
+    "url": "/static/js/main.22c33cfb.chunk.js"
+  },
+  {
+    "revision": "5d16ab4528936a4f5d0b",
+    "url": "/static/js/runtime-main.14b217f4.js"
+  }
+]);
