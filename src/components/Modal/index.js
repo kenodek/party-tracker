3573cf5a8e3e9850b175c5363baa17337a095c92
kenodek <dominik.targosz@protonmail.com>
2020-06-14 17:28:06 +0200

    Added basic modal

---
+++ src/components/Modal/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+
+const Modal = ({ isOpened }) => {
+  const [isOpened, setIsOpened] = useState(false);
+
+  return <div className="modal"></div>;
+};
+
+export default Modal;
